edaaa3066a0810b9d22f7e660c3bd9f038d3d7cd
Kapil Yamkanmardi <kapil.yamkanmardi@kapil.yamkanmardi>
2025-10-14 17:46:14 +0530

    Created component to make draggable notes and save them in the local storage

---
+++ draggable/src/LocalStorage.js
@@ -0,0 +1,29 @@
+//create a hook to load and store data in the local storage
+import { useState, useEffect } from 'react';
+
+function useLocalStorage(key, initialValue) {
+    const [storedValue, setStoredValue] = useState(() => {
+        try {
+            const item = window.localStorage.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
+        }
+        catch (error) {
+            return ""
+        }
+    });
+
+    const updateKeyValue = (key, value) => {
+        try {
+            const valueToStore = value instanceof Function ? value(storedValue) : value;
+            setStoredValue(valueToStore);
+            window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    return [storedValue, updateKeyValue];
+}
+
+export default useLocalStorage;
